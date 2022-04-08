11e30c696688f7d3293b6bf0c83a8f402b6f784d
tiannian <dtiannian@gmail.com>
2022-04-08 15:01:59 +0800

    new data

--- examples/empty/src/main.rs
+++ examples/empty/src/main.rs
@@ -8,4 +8,14 @@ extern "C" fn _gca_unlock_entry() {
     main()
 }
 
+#[no_mangle]
+extern "C" fn _gca_operation_entry() {
+    main()
+}
+
+#[no_mangle]
+extern "C" fn _gca_verifier_entry() {
+    main()
+}
+
 pub fn main() {}
