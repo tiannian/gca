370de3a707e7386c7e83dfb11ce87ace69f56c31
tiannian <dtiannian@gmail.com>
2022-04-14 21:45:43 +0800

    add impl

--- gca/src/types.rs
+++ gca/src/types.rs
@@ -4,8 +4,20 @@ use primitive_types::{H160, H256};
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct BlockHash(pub H256);
 
+impl AsRef<[u8]> for BlockHash {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+}
+
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct Txhash(pub H256);
+
+impl AsRef<[u8]> for Txhash {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+}
 
 #[derive(Debug, Default)]
 pub struct MemoOperation(pub u32);
@@ -28,8 +40,20 @@ pub struct BlockHeight(pub i64);
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct MerkleHash(pub H256);
 
+impl AsRef<[u8]> for MerkleHash {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+}
+
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct Timestamp(pub i64);
 
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct NodeAddress(pub H160);
+
+impl AsRef<[u8]> for NodeAddress {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+}
